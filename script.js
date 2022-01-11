17e13d230210b3e4457e494a4d515f5c97b3d526
robingrit <91528697+robingrit@users.noreply.github.com>
2022-01-11 13:45:44 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Bakgrundsfärgen går från hue=0 på första elementet och ökar sedan med 20 för varje element
+
 for (let i = 0; i < 5; i++) {
   const h3 = document.createElement("h3");
 
@@ -15,12 +15,7 @@ for (let i = 0; i < 5; i++) {
   document.body.append(h3);
 }
 
-/*
-  Hue för första elementet ska alltid vara 180
-  Hue för sista elementet ska alltid vara 300
-  Elementen där emellan ska gradvis skifta från det första till det sista
-  med hue-värdet $${hue}
-*/
+
 
 const div = document.createElement("div");
 div.style.display = "flex";
@@ -29,22 +24,17 @@ div.style.flexDirection = "row";
 div.style.height = "20rem";
 div.style.border = " 1px solid";
 div.style.backgroundColor = "#white";
-//div.style.gap = "28%";
-//div.style.padding = "11%";
+
 div.style.justifyContent = "space-around";
 div.style.alignItems = "center";
 
 document.body.append(div);
-//div.appendChild(innerDiv);
-//div.appendChild(innerDiv2);
-//div.appendChild(innerDiv3);
+
 for (let j = 0; j < 3; j++) {
   const innerDiv = document.createElement("div");
   innerDiv.style.display = "flex";
   innerDiv.style.flexDirection = "column";
-  //innerDiv.style.justifyContent = "space-around";
-  //innerDiv.style.alignItems = "center";
-  //innerDiv.style.fontSize = "66%";
+  
   innerDiv.style.flexBasis = "8%";
   innerDiv.style.height = "70%";
   innerDiv.style.border = " 1px solid";
@@ -57,26 +47,21 @@ for (let j = 0; j < 3; j++) {
     h3.style.height = "100%";
     h3.style.fontSize = "1rem";
 
-    h3.innerText = `${i} `;
-
-    //innerDiv2.style.border = " 1px solid yellow";
-    
-    
-    
+    h3.innerText = `${i} `;    
     if (i == 4) {
-      backcolor = h3.style.backgroundColor = "#a8a8f0";
+      h3.style.backgroundColor = "#a8a8f0";
       h3.style.color = "white";
       h3.style.margin = "0% 15% 0px 15%";
     } else if (i == 0 || i == 2 || i == 6 || i == 8) {
-      backcolor = h3.style.backgroundColor = "black";
-      textcolor = h3.style.color = "white";
+      h3.style.backgroundColor = "black";
+      h3.style.color = "white";
       h3.style.margin = "0% 15% 0px 15%";
       if (i == 0) {
         h3.style.marginTop = "15%";
       }
     } else {
-      backcolor = h3.style.backgroundColor = "white";
-      textcolor = h3.style.color = "black";
+      h3.style.backgroundColor = "white";
+      h3.style.color = "black";
       h3.style.margin = "0% 15% 0px 15%";
       if (i == 9) {
         h3.style.marginBottom = "15%";
@@ -87,16 +72,16 @@ for (let j = 0; j < 3; j++) {
       h3.style.justifyContent = "center";
 
        if (i == 1) {
-         // backcolor = h3.style.backgroundColor = "yellow";
+        
          h3.style.backgroundColor = "#a8a8f0";
          h3.style.color = "white";
        } else if (i == 0 || i == 2 || i == 4 || i == 6 || i == 8) {
-         backcolor = h3.style.backgroundColor = "white";
-         textcolor = h3.style.color = "black";
+         h3.style.backgroundColor = "white";
+         h3.style.color = "black";
        }
        else{
-        backcolor = h3.style.backgroundColor = "black";
-        textcolor = h3.style.color = "white";
+        h3.style.backgroundColor = "black";
+        h3.style.color = "white";
        }
       
     } else if (j == 2) {
@@ -122,9 +107,7 @@ for (let j = 0; j < 3; j++) {
     innerDiv.appendChild(h3);
   }
 
-  //innerDiv2.style.backgroundColor = "white";
-
-  //innerDiv.appendChild(innerDiv2);
+ 
 }
 
 var x = document.querySelectorAll("div > h3");
